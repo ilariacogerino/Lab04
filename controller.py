7bb6ded43afcb8d8bc8b58ed04ff3a990079eb68
ilariacogerino <s312806@studenti.polito.it>
2025-09-08 15:31:29 +0200

    Lab04

--- controller.py
+++ controller.py
@@ -45,6 +45,45 @@ class SpellChecker:
                 return None
 
 
+    def handleLanguage(self, e):
+        self._view.__txtOut.controls.append(
+            ft.Text(value="Language correctly selected: " + self._view.__ddLanguage.value))
+        self._view.page.update()
+
+
+    def handleModality(self, e):
+        self._view.__txtOut.controls.append(
+            ft.Text(value="Search modality correctly selected: " + self._view.__ddModality.value))
+        self._view.page.update()
+
+
+    def handleSpellCheck(self, e):
+        sentence = self._view.__txtInt.value
+        if sentence == "":
+            self._view.__txtOut.controls.clear()
+            self._view.__txtOut.controls.append(ft.Text(value="Add a sentence!"))
+            return
+        language = self._view.__ddLanguage.value
+        modality = self._view.__ddModality.value
+        if language == "":
+            self._view.txtOut.controls.clear()
+            self._view.txtOut.controls.append(ft.Text(value="Select language!"))
+            return
+        if modality == "":
+            self._view.txtOut.controls.clear()
+            self._view.txtOut.controls.append(ft.Text(value="Select modality!"))
+            return
+
+        words, elapsedTime = self.handleSentence(sentence, language, modality)
+
+        self._view.__txtOut.controls.clear()
+        self._view.__txtOut.controls.append(ft.Text("Sentence : " + sentence))
+        self._view.__txtOut.controls.append(ft.Text("Errors: " + words))
+        self._view.__txtOut.controls.append(ft.Text(value="Elapsed time: " + str(elapsedTime)))
+
+        self._view.page.update()
+
+
     def printMenu(self):
         print("______________________________\n" +
               "      SpellChecker 101\n"+
--- view.py
+++ view.py
@@ -7,38 +7,60 @@ class View(object):
         self.page.title = "TdP 2024 - Lab 04 - SpellChecker ++"
         self.page.horizontal_alignment = 'CENTER'
         self.page.theme_mode = ft.ThemeMode.LIGHT
+
         # Controller
         self.__controller = None
+
         # UI elements
         self.__title = None
         self.__theme_switch = None
+        self.__ddLanguage = None
+        self.__ddModality = None
+        self.__txtInt = None
+        self.__btnSpellCheck = None
+        self.__txtOut = None
 
-        # define the UI elements and populate the page
 
     def add_content(self):
-        """Function that creates and adds the visual elements to the page. It also updates
-        the page accordingly."""
         # title + theme switch
         self.__title = ft.Text("TdP 2024 - Lab 04 - SpellChecker ++", size=24, color="blue")
         self.__theme_switch = ft.Switch(label="Light theme", on_change=self.theme_changed)
-        self.page.controls.append(
-            ft.Row(spacing=30, controls=[self.__theme_switch, self.__title, ],
-                   alignment=ft.MainAxisAlignment.START)
-        )
+        row1 = ft.Row(controls=[self.__theme_switch, self.__title], spacing=30, alignment=ft.MainAxisAlignment.CENTER)
+        self.page.add(row1)
 
-        # Add your stuff here
+        # select language
+        self.__ddLanguage = ft.Dropdown(label="Select Language",
+                                        options=[ft.DropdownOption("english"), ft.DropdownOption("italian"), ft.DropdownOption("spanish")],
+                                        on_change=self.__controller.handleLanguage)
+        self.page.add(self.__ddLanguage)
 
-        self.page.add([])
+        # modality + txtIn + Spell check button
+        self.__ddModality = ft.Dropdown(label="Search Modality",
+                                        options=[ft.DropdownOption("Contains"), ft.DropdownOption("Linear"), ft.DropdownOption("Dichotomic")],
+                                        on_change=self.__controller.handleModality)
+        self.__txtInt = ft.TextField(label="Add your sentence here")
+        self.__btnSpellCheck = ft.ElevatedButton(text="Spell Check",
+                                                 on_click=self.__controller.handleSpellCheck)
+        row2 = ft.Row(controls=[self.__ddModality, self.__txtInt, self.__btnSpellCheck])
+        self.page.add(row2)
+
+        # listview to write from controller
+        self.__txtOut = ft.ListView(expand=1, spacing=10)
+        self.page.add(self.__txtOut)
+
 
         self.page.update()
+
 
     def update(self):
         self.page.update()
+
+
     def setController(self, controller):
         self.__controller = controller
+
+
     def theme_changed(self, e):
-        """Function that changes the color theme of the app, when the corresponding
-        switch is triggered"""
         self.page.theme_mode = (
             ft.ThemeMode.DARK
             if self.page.theme_mode == ft.ThemeMode.LIGHT
@@ -47,7 +69,4 @@ class View(object):
         self.__theme_switch.label = (
             "Light theme" if self.page.theme_mode == ft.ThemeMode.LIGHT else "Dark theme"
         )
-        # self.__txt_container.bgcolor = (
-        #     ft.colors.GREY_900 if self.page.theme_mode == ft.ThemeMode.DARK else ft.colors.GREY_300
-        # )
         self.page.update()
